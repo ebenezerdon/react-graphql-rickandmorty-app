3a972a12ecf7a2e25f5870b19e66b74129e19fbf
ebenezerdon <ebenezerdonu@gmail.com>
2021-09-08 21:32:29 +0100

    populate App with cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/client'
+import { GET_ALL_CHARACTERS } from './queries'
+import Card from './Card'
 
-function App() {
+const App = () => {
+  const { loading, error, data } = useQuery(GET_ALL_CHARACTERS, {
+    variables: { page: 3 }
+  })
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error {error.message}</p>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1 className="text-center m-5">Rick and Morty GraphQL App</h1>
       </header>
-    </div>
-  );
+      <div className="row">
+        {data?.characters?.results.map(character =>
+          <Card character={character} key={character.id} />
+        )}
+      </div>
+    </>
+
+  )
 }
 
-export default App;
+export default App
